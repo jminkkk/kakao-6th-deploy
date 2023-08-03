1fbc30429d20e7edc26db728026fb4e54bd1399f
jminkkk <minl741@naver.com>
2023-08-03 12:11:15 +0900

    cart 통합 테스트 구현

--- src/test/java/com/example/kakao/cart/CartRestControllerTest.java
+++ src/test/java/com/example/kakao/cart/CartRestControllerTest.java
@@ -2,6 +2,7 @@ package com.example.kakao.cart;
 
 import com.example.kakao.MyRestDoc;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
@@ -27,14 +28,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public class CartRestControllerTest extends MyRestDoc {
+
     @Autowired
     private ObjectMapper om;
 
     @WithUserDetails(value = "ssarmango@nate.com")
     @Test
+    @DisplayName("장바구니에 아이템을 추가한다.")
     public void addCartList_test() throws Exception {
         // given -> optionId [1,2,16]이 teardown.sql을 통해 들어가 있음
         List<CartRequest.SaveDTO> requestDTOs = new ArrayList<>();
+
         CartRequest.SaveDTO item = new CartRequest.SaveDTO();
         item.setOptionId(3);
         item.setQuantity(5);
@@ -59,6 +63,72 @@ public class CartRestControllerTest extends MyRestDoc {
 
     @WithUserDetails(value = "ssarmango@nate.com")
     @Test
+    @DisplayName("장바구니 추가 시 requestBody에 같은 옵션이 2개 이상 있을 경우 예외 발생.")
+    public void addCartList_fail1_test() throws Exception {
+        // given -> optionId [1,2,16]이 teardown.sql을 통해 들어가 있음
+        List<CartRequest.SaveDTO> requestDTOs = new ArrayList<>();
+
+        CartRequest.SaveDTO item1 = new CartRequest.SaveDTO();
+        item1.setOptionId(3);
+        item1.setQuantity(5);
+        requestDTOs.add(item1);
+
+        CartRequest.SaveDTO item2 = new CartRequest.SaveDTO();
+        item2.setOptionId(3);
+        item2.setQuantity(2);
+        requestDTOs.add(item2);
+
+        String requestBody = om.writeValueAsString(requestDTOs);
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                post("/carts/add")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        );
+
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // verify
+        resultActions.andExpect(jsonPath("$.success").value("false"));
+        resultActions.andExpect(jsonPath("$.error.message").value("요청에 같은 옵션이 여러 개입니다."));
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+
+    @WithUserDetails(value = "ssarmango@nate.com")
+    @Test
+    @DisplayName("장바구니 추가 수량이 0일 경우 장바구니에 추가되지 않는다.")
+    public void addCartList_fail2_test() throws Exception {
+        // given -> optionId [1,2,16]이 teardown.sql을 통해 들어가 있음
+        List<CartRequest.SaveDTO> requestDTOs = new ArrayList<>();
+        CartRequest.SaveDTO item = new CartRequest.SaveDTO();
+        item.setOptionId(3);
+        item.setQuantity(0);
+        requestDTOs.add(item);
+
+        String requestBody = om.writeValueAsString(requestDTOs);
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                post("/carts/add")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        );
+
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // verify
+        resultActions.andExpect(jsonPath("$.success").value("false"));
+        resultActions.andExpect(jsonPath("$.error.message").value("옵션의 수량이 0입니다. 삭제 요청을 해주세요."));
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+    @WithUserDetails(value = "ssarmango@nate.com")
+    @Test
+    @DisplayName("장바구니 목록 조회")
     public void findAll_test() throws Exception {
         // given teardown
 
@@ -87,6 +157,7 @@ public class CartRestControllerTest extends MyRestDoc {
 
     @WithUserDetails(value = "ssarmango@nate.com")
     @Test
+    @DisplayName("장바구니의 수량을 업데이트한다.")
     public void update_test() throws Exception {
         // given -> cartId [1번 5개,2번 1개,3번 5개]가 teardown.sql을 통해 들어가 있음
         List<CartRequest.UpdateDTO> requestDTOs = new ArrayList<>();
@@ -115,6 +186,36 @@ public class CartRestControllerTest extends MyRestDoc {
         resultActions.andExpect(jsonPath("$.response.carts[0].optionName").value("01. 슬라이딩 지퍼백 크리스마스에디션 4종"));
         resultActions.andExpect(jsonPath("$.response.carts[0].quantity").value(10));
         resultActions.andExpect(jsonPath("$.response.carts[0].price").value(100000));
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+    @WithUserDetails(value = "ssarmango@nate.com")
+    @Test
+    @DisplayName("존재하지 않는 장바구니에 업데이트 요청이 올 시 오류가 발생한다.")
+    public void update_fail_test() throws Exception {
+        // given -> cartId [1번 5개,2번 1개,3번 5개]가 teardown.sql을 통해 들어가 있음
+        List<CartRequest.UpdateDTO> requestDTOs = new ArrayList<>();
+        CartRequest.UpdateDTO item = new CartRequest.UpdateDTO();
+        item.setCartId(4);
+        item.setQuantity(10);
+        requestDTOs.add(item);
+
+        String requestBody = om.writeValueAsString(requestDTOs);
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                post("/carts/update")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        );
+
+        // eye
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // verify
+        resultActions.andExpect(jsonPath("$.success").value("false"));
+        resultActions.andExpect(jsonPath("$.error.message").value("해당 장바구니를 찾을 수 없습니다."));
         resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
     }
 
